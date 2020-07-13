bd12f42a5319dfd00f4039d05e3834e7ae14fa06
GrossWhiteRabbit <vowa.kononenko@gmail.com>
2020-07-13 13:22:55 +0300

    first commit

---
+++ js/skript.js
@@ -0,0 +1,16 @@
+"use strict";
+let incr  = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++, decr--)
+
+console.log(10%6);
+
+console.log(3*9 === '27');
+
+const isChecked = false,
+    isClose = false;
+console. log(isChecked || isClose);
